769ab81e024359db1778de9952aa870206454a24
Praise-Bismark <bismarkjonesoduro@gmail.com>
2025-06-21 17:33:59 +0000

    shopMart

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar.jsx'; // Adjust the path as necessary
+import Home from './components/Home.jsx';
+import About from './components/About.jsx';
+import Product from './components/Product.jsx';
+import Contact from './components/Contact.jsx';
+import Details from './components/Details.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/product' element={<Product />} />
+          <Route path='/product/:id' element={<Details />} />
+          {/* <Route path='/navbar' element={<Navbar />} /> */}
+          {/* <Route path='*' element={<div>404 Not Found</div>} /> */}
+          {/* Add more routes as needed */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
